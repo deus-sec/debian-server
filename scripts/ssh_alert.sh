2c27f0824434be55eb26bcfb51beaba1b800c402
deus-sec <architectes@proton.me>
2026-04-14 21:32:56 +0200

    Update ssh_alert.sh

--- scripts/ssh_alert.sh
+++ scripts/ssh_alert.sh
@@ -1,1 +1,10 @@
-
+cat > /usr/local/bin/ssh_alert.sh << 'EOF'
+#!/bin/bash
+cat > /tmp/ssh_alert.txt << ALERTEOF
+PAM_TYPE=${PAM_TYPE}
+PAM_USER=${PAM_USER}
+PAM_RHOST=${PAM_RHOST}
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+ALERTEOF
+EOF
+chmod +x /usr/local/bin/ssh_alert.sh
